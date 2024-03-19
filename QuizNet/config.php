9e85f89e1ded37cbec4e23618945c8975842ef51
Marosi2431 <marosi2431@gmail.com>
2024-03-19 18:59:39 +0100

    dodatkowe pliki

---
+++ QuizNet/config.php
@@ -0,0 +1,21 @@
+<?php
+ 
+$mysqli = new mysqli("localhost","root","","quiz");
+
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+ 
+function codepass($password) {
+    return sha1(md5($password).'#!%Rgd64');
+}
+
+session_start();
+
+if(!isset($_SESSION['logged'])) {
+    $_SESSION['logged'] = false;
+    $_SESSION['user_id'] = -1;
+}
+
+?>
