eb98262a3956a51670f355240a1d7b50d6f58482
Marosi2431 <marosi2431@gmail.com>
2024-03-19 18:56:59 +0100

    Logowanie, rejestracja

--- QuizNet/index.php
+++ QuizNet/index.php
@@ -1,3 +1,9 @@
+<?php
+
+include("config.php");
+
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -14,8 +20,30 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
-        <?php include 'navbar.php'; ?>
-        <p>Jestes na indexie</p>
+        <?php include 'navbar.php'; 
+		
+		if($_SESSION['logged']){
+			
+			$sql = "SELECT * FROM users WHERE id = '{$_SESSION['user_id']}'";
+			$result = $mysqli -> query($sql);
+			$row = $result -> fetch_assoc();
+			if($row['admin'] == 1) {
+				echo '<p>Witaj '.$row['login'].'. Jesteś administratorem, brawo! <a href="logout.php">Wyloguj sie</a></p>';
+			}else{
+				echo '<p>Witaj '.$row['login'].'. Zapraszamy do zabawy! <a href="logout.php">Wyloguj sie</a></p>';
+			}
+			
+		}else{
+			echo '<p>Zaloguj się bądź zarejestruj</p>';
+		}
+		
+		?>
 </body>
 <script src="script.js"></script>
 </html>
+
+<?php
+
+$mysqli -> close();
+
+?>
--- QuizNet/login.php
+++ QuizNet/login.php
@@ -5,17 +5,60 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script> 
-<?php include 'navbar.php'; ?>
+<?php include 'navbar.php'; 
+include("config.php");
+
+if(!$_SESSION['logged']){
+	if(isset($_POST['reg_login'])){
+		if($_POST['reg_pass'] == $_POST['reg_pass2']){
+			$sql = "SELECT login FROM users WHERE login = '{$_POST['reg_login']}'";
+			$result = $mysqli -> query($sql);
+			if($result -> num_rows == 0){
+				$pass = codepass($_POST['reg_pass']);
+				$sql = "INSERT INTO `users` (`login`, `password`) VALUES ('{$_POST['reg_login']}', '".$pass."')";
+				
+				if ($mysqli -> query($sql)) {
+					echo '<p>Pomyślnie zarejestrowano! Możesz się zalogować.</p>';
+				}else{
+					echo '<p>Błąd dodania:</p>';
+					echo $mysqli -> error;
+				}
+			}else{
+				echo '<p>Istnieje już użytkownik z takim loginem</p>';
+			}
+		}else{
+			echo '<p>Hasła nie są takie same!</p>';
+		}
+	}
+	
+	if(isset($_POST['login'])){
+		$pass = codepass($_POST['password']);
+		$sql = "SELECT `id` FROM `users` WHERE `login` = '{$_POST['login']}' AND `password` = '".$pass."' LIMIT 1";
+		$result = $mysqli -> query($sql);
+		if($result -> num_rows > 0){
+			$row = $result -> fetch_assoc();
+			$_SESSION['logged'] = true;
+            $_SESSION['user_id'] = $row['id'];
+			echo '<p>Poprawnie zalogowano</p>';
+		}else{
+			echo '<p>Błędny login lub hasło!</p>';
+		}
+	}
+}
+
+?>
 <!-- Div odpowiedzialny za widocznośc Loginu -->
 <div class="login">
 <img src="/img/QuizNet.png" alt="Logo">
     <h1>Logowanie</h1>
     <p>Podaj login oraz hasło</p>
-    <input type="text" class="login-label" placeholder="Login">
-    <input type="password" class="password-label" placeholder="Hasło">
+	<form action="login.php" method="POST">
+    <input type="text" class="login-label" name="login" placeholder="Login">
+    <input type="password" class="password-label" name="password" placeholder="Hasło">
     <div class="confirm-button">
-    <button class="confirm">Zaloguj</button>
+    <button class="confirm" type="submit">Zaloguj</button>
     </div>
+	</form>
     <!-- Funkcja podmieniająca wyświetlaną zawartość -->
     <p>Nie masz konta? <a href="#" onclick="showRegistration()">Zarejestruj się!</a></p> 
 </div>
@@ -23,13 +66,21 @@
     <img src="/img/QuizNet.png" alt="Logo">
 <h1>Rejestracja</h1>
     <p>Podaj login oraz hasło</p>
-    <input type="text" class="login-label" placeholder="Login">
-    <input type="password" class="password-label" placeholder="Hasło">
-    <input type="password" class="password-confirm-label" placeholder="Potwierdź hasło">
+	<form action="login.php" method="POST">
+    <input type="text" class="login-label" name="reg_login" placeholder="Login">
+    <input type="password" class="password-label" name="reg_pass" placeholder="Hasło">
+    <input type="password" class="password-confirm-label" name="reg_pass2" placeholder="Potwierdź hasło">
     <div class="confirm-button">
-        <button class="confirm">Zarejestruj</button>
+        <button class="confirm" type="submit">Zarejestruj</button>
     </div>
+	</form>
     <!-- Funkcja podmieniająca wyświetlaną zawartość -->
     <p>Masz już konto? <a href="#" onclick="showLogin()">Zaloguj się!</a></p> 
 </div>
 <script src="script.js"></script>
+
+<?php
+
+$mysqli -> close();
+
+?>
