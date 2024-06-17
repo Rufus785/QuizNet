1f2d645235a8667425df22045c60dc3544dfb90b
smlew <38687747+smlew@users.noreply.github.com>
2024-06-17 17:41:27 +0000

    Delete old files

--- QuizNet/script.js
+++
@@ -1,9 +0,0 @@
-function showRegistration() {
-    document.querySelector('.login').style.display = 'none';
-    document.querySelector('.registration').style.display = 'block';
-}
-
-function showLogin() {
-    document.querySelector('.registration').style.display = 'none';
-    document.querySelector('.login').style.display = 'block';
-}
